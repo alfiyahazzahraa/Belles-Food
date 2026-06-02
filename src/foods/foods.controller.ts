8b220547e194113224648800c8dd615ce1cef401
Alfiyah fadantya zahra <alfiyahfadantyaazzahra@email.com>
2026-06-02 09:31:45 +0700

    fix: add ApiBearerAuth decorator to foods controller

--- src/foods/foods.controller.ts
+++ src/foods/foods.controller.ts
@@ -1,11 +1,14 @@
-import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards,ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param, UseGuards, ParseIntPipe } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { FoodsService } from './foods.service';
 import { CreateFoodDto } from './dto/create-food.dto';
 import { UpdateFoodDto } from './dto/update-food.dto';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'; 
 
+@ApiTags('Foods')     
+@ApiBearerAuth()       
 @Controller('foods')
 export class FoodsController {
   constructor(private readonly foodsService: FoodsService) {}
